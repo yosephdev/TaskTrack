5a2a7a70b7ac611291806883d02fab7a093b2c0d
Yoseph Berhane <yosephbet@gmail.com>
2023-02-28 20:38:12 +0100

    change main.js function

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -3,87 +3,32 @@
 var attGora = document.getElementById('att_gora');
 var laggTill = document.getElementById('lagg_till_btn');
 var fardiga = document.getElementById('fardiga');
-/* Scenario 1 (Lägga till) */
+var felNamn = document.getElementById('fel_namn');
 
-laggTill.addEventListener("click", function (e) {
-  e.preventDefault();
-  var attGoraList = document.createElement('li');
-  var attGoraSyssla = document.createElement('input');
-  attGoraSyssla.value = document.getElementById('syssla').value;
-  attGoraSyssla.setAttribute('type', 'text');
-  attGoraSyssla.setAttribute('id', 'attGoraListItem');
-  attGoraSyssla.setAttribute('readonly', '');
-  /* Scenario 2 (Redigera syssla) */
+function addTask() {
+  var task = document.getElementById('syssla').value.trim();
 
-  var btnAndra1 = document.createElement('button');
-  btnAndra1.setAttribute('type', 'button');
-  btnAndra1.setAttribute('id', 'andra');
-  btnAndra1.innerHTML = "Ändra";
-  btnAndra1.addEventListener('click', function () {
-    attGoraSyssla.setAttribute('readonly', '');
-    attGoraSyssla.removeAttribute('readonly');
-    attGoraList.appendChild(btnAndra1);
-    attGora.appendChild(attGoraList);
-  });
-  /* Scenario 3 (Radera syssla) */
-
-  var btnRadera = document.createElement('button');
-  btnRadera.setAttribute('type', 'button');
-  btnRadera.setAttribute('id', 'radera');
-  btnRadera.innerHTML = "Radera";
-  btnRadera.addEventListener('click', function () {
-    attGoraList.remove();
-  });
-  /* Scenario 4 (Markera syssla som klar) */
-
-  var btnFardig = document.createElement('button');
-  btnFardig.setAttribute('type', 'button');
-  btnFardig.setAttribute('id', 'fardig');
-  btnFardig.innerHTML = "Färdig";
-  btnFardig.addEventListener('click', function () {
-    var fardigaList = document.createElement('li');
-    var fardigaSyssla = document.createElement('input');
-    fardigaSyssla.value = attGoraSyssla.value;
-    fardigaSyssla.setAttribute('type', 'text');
-    fardigaSyssla.setAttribute('id', 'fardigaListItem');
-    fardigaSyssla.setAttribute('readonly', '');
-    var btnAndra2 = document.createElement('button');
-    btnAndra2.setAttribute('type', 'button');
-    btnAndra2.setAttribute('id', 'andra');
-    btnAndra2.innerHTML = "Ändra";
-    btnAndra2.addEventListener('click', function () {
-      fardigaSyssla.setAttribute('readonly', '');
-      fardigaSyssla.removeAttribute('readonly');
-      fardigaList.appendChild(btnAndra2);
-      fardiga.appendChild(fardigaList);
-    });
-    btnRadera.addEventListener('click', function () {
-      fardigaList.remove();
-    });
-    fardigaList.appendChild(fardigaSyssla);
-    fardigaList.appendChild(btnAndra2);
-    fardigaList.appendChild(btnRadera);
-    fardiga.appendChild(fardigaList);
-    attGoraSyssla.remove();
-    btnAndra1.remove();
-    btnFardig.remove();
-    attGoraList.remove();
-  });
-  attGoraList.appendChild(attGoraSyssla);
-  attGoraList.appendChild(btnAndra1);
-  attGoraList.appendChild(btnRadera);
-  attGoraList.appendChild(btnFardig);
-  attGora.appendChild(attGoraList);
-});
-/* Visa lämpliga felmeddelande via HTML */
-
-function errorMessage(e) {
-  var error = document.getElementById("fel_namn");
-
-  if (document.getElementById("syssla").value == "") {
-    error.innerHTML = "<span style='color: red; font-size: 1.2em;'>" + "Får ej skapa tomma sysslar</span>";
-    e.preventDefault();
+  if (task === '') {
+    felNamn.innerText = 'Fyll i en syssla först.';
   } else {
-    error.innerHTML = "";
+    felNamn.innerText = '';
+    var taskItem = createTaskItem(task);
+    attGora.appendChild(taskItem);
+    document.getElementById('syssla').value = '';
   }
 }
+
+function createTaskItem(task) {
+  var taskItem = document.createElement("li");
+  taskItem.classList.add("flex", "items-center", "justify-between", "py-2", "px-4", "my-2", "bg-white", "rounded", "shadow", "hover:shadow-lg");
+  taskItem.innerHTML = "\n<input class=\"w-full text-gray-700 rounded-md px-4 py-2 border border-gray-300 shadow-sm focus:outline-none focus:ring-2\nfocus:ring-purple-600 focus:border-transparent\" type=\"text\" value=\"".concat(task, "\" readonly>\n<button class=\"ml-2 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline\"\nonclick=\"markTaskAsDone(this)\">Klar</button>\n");
+  return taskItem;
+}
+
+function markTaskAsDone(element) {
+  var taskItem = element.parentNode;
+  taskItem.remove();
+  fardiga.appendChild(taskItem);
+}
+
+laggTill.addEventListener("click", addTask);
--- js/main.js
+++ js/main.js
@@ -1,99 +1,47 @@
 let attGora = document.getElementById('att_gora');
 let laggTill = document.getElementById('lagg_till_btn');
 let fardiga = document.getElementById('fardiga');
+let felNamn = document.getElementById('fel_namn');
 
-
-/* Scenario 1 (Lägga till) */
-
-laggTill.addEventListener("click", function(e) {
-    e.preventDefault();
-
-    let attGoraList = document.createElement('li');
-    let attGoraSyssla = document.createElement('input');
-    attGoraSyssla.value = document.getElementById('syssla').value;
-    attGoraSyssla.setAttribute('type', 'text');
-    attGoraSyssla.setAttribute('id', 'attGoraListItem');
-    attGoraSyssla.setAttribute('readonly', '');
-
-    /* Scenario 2 (Redigera syssla) */
-
-    let btnAndra1 = document.createElement('button');
-    btnAndra1.setAttribute('type', 'button');
-    btnAndra1.setAttribute('id', 'andra');
-    btnAndra1.innerHTML = "Ändra";
-    btnAndra1.addEventListener('click', function() {
-        attGoraSyssla.setAttribute('readonly', '');
-        attGoraSyssla.removeAttribute('readonly');
-        attGoraList.appendChild(btnAndra1);
-        attGora.appendChild(attGoraList);        
-    });
-
-	/* Scenario 3 (Radera syssla) */
-	
-    let btnRadera = document.createElement('button');
-    btnRadera.setAttribute('type', 'button');
-    btnRadera.setAttribute('id', 'radera');
-    btnRadera.innerHTML = "Radera";
-    btnRadera.addEventListener('click', function() {
-        attGoraList.remove();
-    });
-
-    /* Scenario 4 (Markera syssla som klar) */
-
-    let btnFardig = document.createElement('button');
-
-    btnFardig.setAttribute('type', 'button');
-    btnFardig.setAttribute('id', 'fardig');
-    btnFardig.innerHTML = "Färdig";
-    btnFardig.addEventListener('click', function() {
-        let fardigaList = document.createElement('li');
-        let fardigaSyssla = document.createElement('input');
-        fardigaSyssla.value = attGoraSyssla.value;
-        fardigaSyssla.setAttribute('type', 'text');
-        fardigaSyssla.setAttribute('id', 'fardigaListItem');
-        fardigaSyssla.setAttribute('readonly', '');
-
-        let btnAndra2 = document.createElement('button');
-        btnAndra2.setAttribute('type', 'button');
-        btnAndra2.setAttribute('id', 'andra');
-        btnAndra2.innerHTML = "Ändra";
-        btnAndra2.addEventListener('click', function() {
-            fardigaSyssla.setAttribute('readonly', '');
-            fardigaSyssla.removeAttribute('readonly');
-            fardigaList.appendChild(btnAndra2);
-            fardiga.appendChild(fardigaList);
-        });
-
-        btnRadera.addEventListener('click', function() {
-            fardigaList.remove();
-        });
-
-        fardigaList.appendChild(fardigaSyssla);
-        fardigaList.appendChild(btnAndra2);
-        fardigaList.appendChild(btnRadera);
-        fardiga.appendChild(fardigaList);
-
-        attGoraSyssla.remove();
-        btnAndra1.remove();
-        btnFardig.remove();
-        attGoraList.remove();
-    });
-
-    attGoraList.appendChild(attGoraSyssla);
-    attGoraList.appendChild(btnAndra1);
-    attGoraList.appendChild(btnRadera);
-    attGoraList.appendChild(btnFardig);
-    attGora.appendChild(attGoraList);
-});
-
-/* Visa lämpliga felmeddelande via HTML */
-
-function errorMessage(e) {
-    let error = document.getElementById("fel_namn");
-    if (document.getElementById("syssla").value == "") {
-        error.innerHTML = "<span style='color: red; font-size: 1.2em;'>" + "Får ej skapa tomma sysslar</span>";
-        e.preventDefault();
-    } else {
-        error.innerHTML = "";
-    }
+function addTask() {
+let task = document.getElementById('syssla').value.trim();
+if (task === '') {
+felNamn.innerText = 'Fyll i en syssla först.';
+} else {
+felNamn.innerText = '';
+let taskItem = createTaskItem(task);
+attGora.appendChild(taskItem);
+document.getElementById('syssla').value = '';
 }
+}
+
+function createTaskItem(task) {
+  let taskItem = document.createElement("li");
+  taskItem.classList.add(
+    "flex",
+    "items-center",
+    "justify-between",
+    "py-2",
+    "px-4",
+    "my-2",
+    "bg-white",
+    "rounded",
+    "shadow",
+    "hover:shadow-lg"
+  );
+  taskItem.innerHTML = `
+<input class="w-full text-gray-700 rounded-md px-4 py-2 border border-gray-300 shadow-sm focus:outline-none focus:ring-2
+focus:ring-purple-600 focus:border-transparent" type="text" value="${task}" readonly>
+<button class="ml-2 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+onclick="markTaskAsDone(this)">Klar</button>
+`;
+  return taskItem;
+}
+
+function markTaskAsDone(element) {
+  let taskItem = element.parentNode;
+  taskItem.remove();
+  fardiga.appendChild(taskItem);
+}
+
+laggTill.addEventListener("click", addTask);
